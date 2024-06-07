3f316ea5015642040da6edc607feda7c660da405
AulyaNuraini <aulyanuraini17@gmail.com>
2024-06-07 15:02:53 +0700

    dashboard

---
+++ Perpustakaan/database/migrations/2024_06_06_135533_create_peminjaman_tabel.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('peminjaman', function (Blueprint $table) {
+            $table->id();
+            $table->date('tanggal_peminjaman');
+            $table->date('tanggal_pengembalian');
+            $table->unsignedBigInteger('member_id');
+            $table->foreign('member_id')->references('id')->on('member')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('peminjaman');
+    }
+};
